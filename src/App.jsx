150958c9db688846e5a413dae43f0bd02a5dd260
MirazZim <miraz.zim.38@gmail.com>
2025-01-10 15:26:06 +0600

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import ReusableForm from './Components/ReusableForm/ReusableForm'
+// import ReusableForm from './Components/ReusableForm/ReusableForm'
 // import HookForm from './Components/HookForm/HookForm'
 // import RefForm from './Components/RefForm/RefForm'
 // import SimpleForm from './Components/SimpleForm/SimpleForm'
@@ -8,26 +8,26 @@ import ReusableForm from './Components/ReusableForm/ReusableForm'
 
 function App() {
   
-  const handleSignUpSubmit = data => {
-    console.log('Sign Up data',data);
-    }
+  // const handleSignUpSubmit = data => {
+  //   console.log('Sign Up data',data);
+  //   }
 
-  const handleProfileSubmit = data => {
-    console.log('Update Profile',data);
-    }
+  // const handleProfileSubmit = data => {
+  //   console.log('Update Profile',data);
+  //   }
     
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
 
       {/* <HookForm></HookForm> */}
 
-      <ReusableForm FormTitle = {'Sign Up'} handleSubmit = {handleSignUpSubmit}></ReusableForm>
-      <ReusableForm FormTitle = {'Profile Update'} submitBtnText= {'Update'} handleSubmit={handleProfileSubmit}></ReusableForm>
+      {/* <ReusableForm FormTitle = {'Sign Up'} handleSubmit = {handleSignUpSubmit}></ReusableForm>
+      <ReusableForm FormTitle = {'Profile Update'} submitBtnText= {'Update'} handleSubmit={handleProfileSubmit}></ReusableForm> */}
       
       
     </>
